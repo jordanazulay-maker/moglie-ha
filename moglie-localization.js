87da1b104cfad5481a9e1083e441eb1564aeb704
jordanazulay-maker <jordanazulay@gmail.com>
2026-05-08 21:07:02 -0500

    Add files via upload

---
+++ moglie-localization.js
@@ -0,0 +1,62 @@
+export const MOGLIE_TRANSLATIONS = {
+  "en": { // English
+    "off": "Moglie is stranded. The WAN connection has been lost!",
+    "rain": "Looks like rain, grabbing my coat!",
+    "cold": "Brrr! It's freezing out there!",
+    "hot": "It's boiling! Need a banana smoothie.",
+    "dis": "System's off! The troop is relaxing.",
+    "home": "The troop is home. Primates are on patrol.",
+    "away": "The troop is away. Watching the trees!",
+    "night": "The troop is sleeping in the canopy..."
+  },
+  "he": { // Hebrew (RTL)
+    "off": "מוליה מנותק. חיבור האינטרנט אבד!",
+    "rain": "נראה שעומד לרדת גשם, לוקח את המעיל שלי!",
+    "cold": "בררר! קפוא בחוץ!",
+    "hot": "לוהט בחוץ! צריך שייק בננה קר.",
+    "dis": "המערכת כבויה! הלהקה נחה.",
+    "home": "הלהקה בבית. הקופים בשמירה.",
+    "away": "הלהקה מחוץ לבית. שומרים על העצים!",
+    "night": "הלהקה ישנה בצמרות העצים..."
+  },
+  "es": { // Spanish
+    "off": "¡Moglie está desconectado! Se perdió la conexión.",
+    "rain": "¡Parece que va a llover, buscaré mi abrigo!",
+    "cold": "¡Brrr! ¡Hace un frío polar afuera!",
+    "hot": "¡Qué calor! Necesito un batido de banana.",
+    "dis": "¡Sistema apagado! La tropa está descansando.",
+    "home": "La tropa está en casa. Monos patrullando.",
+    "away": "La tropa está fuera. ¡Vigilando los árboles!",
+    "night": "La tropa está durmiendo en la copa de los árboles..."
+  },
+  "fr": { // French
+    "off": "Moglie est isolé. La connexion internet est perdue !",
+    "rain": "On dirait qu'il pleut, je prends mon manteau !",
+    "cold": "Brrr ! Il fait glacial dehors !",
+    "hot": "C'est la canicule ! Besoin d'un smoothie à la banane.",
+    "dis": "Système désactivé ! La troupe se repose.",
+    "home": "La troupe est à la maison. Patrouille en cours.",
+    "away": "La troupe est partie. Surveillance des arbres !",
+    "night": "La troupe dort dans la canopée..."
+  },
+  "de": { // German
+    "off": "Moglie ist gestrandet. Internetverbindung verloren!",
+    "rain": "Es sieht nach Regen aus, ich hole meine Jacke!",
+    "cold": "Brrr! Es ist eiskalt draußen!",
+    "hot": "Es ist kochend heiß! Brauche einen Bananen-Smoothie.",
+    "dis": "System aus! Die Truppe entspannt sich.",
+    "home": "Die Truppe ist zu Hause. Affen auf Patrouille.",
+    "away": "Die Truppe ist weg. Wir bewachen die Bäume!",
+    "night": "Die Truppe schläft in den Baumkronen..."
+  },
+  "it": { // Italian
+    "off": "Moglie è isolato. Connessione internet persa!",
+    "rain": "Sembra che piova, prendo il mio cappotto!",
+    "cold": "Brrr! Si gela fuori!",
+    "hot": "Si bolle! Ho bisogno di uno smoothie alla banana.",
+    "dis": "Sistema spento! Il branco si riposa.",
+    "home": "Il branco è a casa. Scimmie in pattuglia.",
+    "away": "Il branco è fuori. Sorvegliamo gli alberi!",
+    "night": "Il branco dorme sulla chioma degli alberi..."
+  }
+};
